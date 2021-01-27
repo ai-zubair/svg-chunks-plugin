6e59244b9595970ba3c12e325b9fb286dfd834d0
Zubair Bashir <zubair.bashirzubair@gmail.com>
2021-01-27 17:15:54 +0530

    Fix webpack chunk name issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 module.exports = ({ mode }) => ({
   mode,
-  devtool: 'source-map',
   context: __dirname + '/src',
   entry: './ClientApp.tsx',
   output: {
-    chunkFilename: '[name].chunk.js',
+    filename: 'bundle.js',
+    chunkFilename: '[name].chunk.[chunkhash].js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
@@ -28,6 +28,7 @@ module.exports = ({ mode }) => ({
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './index.html'),
+      scriptLoading: 'defer',
     }),
     new webpack.ProgressPlugin(),
   ],
